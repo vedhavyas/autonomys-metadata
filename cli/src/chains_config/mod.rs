86866d4f9a405ce59c0af7ee075c7dfcb36c8636
pgolovkin <pavel@novawallet.io>
2023-04-18 09:34:30 +0300

    Added Crust and Bittensor

--- cli/src/chains_config/mod.rs
+++ cli/src/chains_config/mod.rs
@@ -43,7 +43,7 @@ pub(crate) struct ChainNode {
 const EXCLUDE_CHAINS: [&str; 3] = [
     "Arctic Relay Testnet",
     "Aleph Zero Testnet", //TODO name matches with mainnet and will override it
-    "Crust",              //Specs(Base58PrefixMismatch { specs: 66, meta: 88 })
+    "Hashed Network",              // Specs(Base58PrefixMismatch { specs: 9072, meta: 42 })
 ];
 
 pub(crate) fn update_chains_config(chains_opts: ChainsOpts) -> Result<()> {
